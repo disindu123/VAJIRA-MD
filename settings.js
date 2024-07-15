eccb64d62cdbf9bd60c1d303a9a01fa2ee037642
disindu123 <disindudamsandu@gmail.com>
2024-07-15 17:35:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94775704025' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=2UdxzbJZ#PURzDrDiGYIRTPkC8APKQYUCCTiaZYe7p55hzUHPl20' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=mU9SSZaI#QStXnxWwEoHsiM21bmezlvIvyEg7964t3yG2zvyik5c' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94775704025,94775704025" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
